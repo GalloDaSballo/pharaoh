b16598838314679dc33b627b7ab3fdb24cccec64
Entreprenerd <alex@entreprenerd.xyz>
2023-05-26 17:05:13 +0200

    Initial commit from Create Next App

---
+++ components/TextRegex.tsx
@@ -0,0 +1,13 @@
+
+export default function TextRegex() {
+  const [text, setText] = useState()
+  return (
+    <>
+
+      <div>
+        <textarea onChange={}
+        
+      </div>
+    </>
+  )
+}
